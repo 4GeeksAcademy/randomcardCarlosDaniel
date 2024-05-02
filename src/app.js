bf3693ee887522f08c0b07714d4b42dab83f9508
Carlos Daniel Hurtado <108777036+carlosdaniel236@users.noreply.github.com>
2024-05-02 01:42:03 +0000

    ya

--- src/app.js
+++ src/app.js
@@ -23,19 +23,21 @@ window.onload = function() {
     "K"
   ];
 
-  const generarNuevaCarta = () => {
-    let palo = palos[Math.floor(Math.random() * palos.length)];
-    let valor = valores[Math.floor(Math.random() * valores.length)];
-    let carta = `${valor} ${palo}`;
+  let palo = palos[Math.floor(Math.random() * palos.length)];
+  let valor = valores[Math.floor(Math.random() * valores.length)];
+  let palo1 = document.getElementById("palo1");
+  let valorCentro = document.getElementById("valor");
+  let palo2 = document.getElementById("palo2");
 
-    const cartaElement = document.getElementById("carta");
-    cartaElement.classList.remove(...cartaElement.classList); // Remove existing classes
-    cartaElement.classList.add("card", palo); // Add new classes
-    cartaElement.textContent = carta;
-  };
+  palo1.innerText = palo;
+  palo2.innerText = palo;
+  valorCentro.innerText = valor;
 
-  document
-    .getElementById("generarNuevaCarta")
-    .addEventListener("click", generarNuevaCarta);
-  console.log("Hello Rigo from the console!");
+  const cartaElement = document.getElementById("carta");
+  cartaElement.classList.remove(...cartaElement.classList); // Remove existing classes
+  cartaElement.classList.add("card", palo); // Add new classes
+  cartaElement.textContent = carta;
+
+  if (palo1 === "♦" || palo2 === "♥") {
+  }
 };
